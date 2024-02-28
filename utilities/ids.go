f211a969d596181acc433788e998f8116bd87d4d
Justin Bornais <justin.bornais@gmail.com>
2024-02-28 11:38:20 -0500

    Delete utilities/ids.go

--- utilities/ids.go
+++
@@ -1,9 +0,0 @@
-package utilities
-
-var (
-	URL = "https://graph.facebook.com/v17.0/233298403205906/messages"
-	// Replace with your actual token.
-	Token = "EAALMF3jOoVgBOZBfrFHIu0lsAcudLRiFibv7i3Ms221p4fKHaCYmuaHnGZCvVvrB1y4XbDKrYhLZA52vZA4301Swo68u8UPn4dPSPm64k3mgx0Dvs69anOZBkoYc2ZBMnge2cKdPPZAdwSncvIQbZBnpmD5QlSz23MeXHkcwNZBkBiRpJ8SxxSeIauOnlJQJmpDg7tmFg38OBbAbEhck9sKUZD"
-	// Replace with the recipient's phone number.
-	To = "+15199912176"
-)
