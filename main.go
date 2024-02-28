bfd87b71d9320a1d175766693c18b11ee1dd827f
Matthew Belanger <matt_belanger1@outlook.com>
2024-02-28 10:51:56 -0500

    updated token

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 var (
 	url = "https://graph.facebook.com/v17.0/233298403205906/messages"
 	// Replace with your actual token
-	token = "EAALMF3jOoVgBO94ufYYMxnT49tP05HaB9gf89DRvdMHzJy6X9QiWJFq0TWZBdgKtLUgH1NSarA9t6q75qP7xmV8VQO0oEbUKNZAcTNigA4PamWFQwt0nNCZCR5OKJAEa06Y6HYcOhLajAFABRInFUAsYb8swKJdXXAJi1CFgzrYs4h65rTM87ZAxCqNBkBGuChmc4OfgkG8Trze2WSeP"
+	token = "EAALMF3jOoVgBOzwXHcNrEEbTRoewuJjc8hu24dH0xkmliiVz8kYSjpiY4j5YvGBdpcTuwtNSgokBGei8ZBM1c4B2tjD3z2JlGdJLbZA1nhpxs87PlrmsUL2e0pUiGXYAOsZA2KP4a2AEOk9oejM9GNfqFiARbTcgF3ao8rGxdhQmGIsHp0ZAeKQU9QuXugGIPCVQPw1szA1iXmbAkPYZD"
 	// Replace with the recipient's phone number
 	to = "+15199912176"
 )
