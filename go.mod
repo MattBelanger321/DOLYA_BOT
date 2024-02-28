ceb8404f288f12a3c27aa6f1df880d7f5ddf13d3
justinbornais <justin.bornais@gmail.com>
2024-02-28 10:45:00 -0500

    Created struct for messages, go.mod and error checker.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mattbelanger321/dolya-bot
+
+go 1.20
--- main.go
+++ main.go
@@ -2,42 +2,48 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/mattbelanger321/dolya-bot/utilities"
+)
+
+var (
+	url = "https://graph.facebook.com/v17.0/233298403205906/messages"
+	// Replace with your actual token
+	token = "EAALMF3jOoVgBO94ufYYMxnT49tP05HaB9gf89DRvdMHzJy6X9QiWJFq0TWZBdgKtLUgH1NSarA9t6q75qP7xmV8VQO0oEbUKNZAcTNigA4PamWFQwt0nNCZCR5OKJAEa06Y6HYcOhLajAFABRInFUAsYb8swKJdXXAJi1CFgzrYs4h65rTM87ZAxCqNBkBGuChmc4OfgkG8Trze2WSeP"
+	// Replace with the recipient's phone number
+	to = "+15199912176"
 )
 
 func main() {
-	url := "https://graph.facebook.com/v17.0/233298403205906/messages"
-	token := "EAALMF3jOoVgBO94ufYYMxnT49tP05HaB9gf89DRvdMHzJy6X9QiWJFq0TWZBdgKtLUgH1NSarA9t6q75qP7xmV8VQO0oEbUKNZAcTNigA4PamWFQwt0nNCZCR5OKJAEa06Y6HYcOhLajAFABRInFUAsYb8swKJdXXAJi1CFgzrYs4h65rTM87ZAxCqNBkBGuChmc4OfgkG8Trze2WSeP" // Replace with your actual token
-	to := "+15199912176"                                                                                                                                                                                                             // Replace with the recipient's phone number
 
-	jsonData := []byte(`{
-		"messaging_product": "whatsapp",
-		"recipient_type": "individual",
-		"to": "` + to + `",
-		"type": "text",
-		"text": {
-			"preview_url": true,
-			"body": "DOLYA BOT ACTIVATED"
-		}
-	}`)
+	message := utilities.WhatsAppMessage{
+		MessagingProduct: "whatsapp",
+		RecipientType:    "individual",
+		To:               to,
+		Type:             "text",
+		Text: utilities.TextStruct{
+			PreviewURL: true,
+			Body:       "DOLYA BOT ACTIVATED",
+		},
+	}
+
+	// Turn the message struct into a JSON object.
+	jsonData, err := json.Marshal(message)
+	utilities.CheckError("Error marshalling the JSON", err)
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return
-	}
+	utilities.CheckError("Error creating request", err)
 
 	req.Header.Set("Authorization", "Bearer "+token)
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error making request:", err)
-		return
-	}
+	utilities.CheckError("Error making request", err)
 	defer resp.Body.Close()
 
 	fmt.Println("Response Status:", resp.Status)
